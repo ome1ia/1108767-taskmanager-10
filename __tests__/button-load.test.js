d51699fffa308ea010fe9e717231259304fefd87
ome1ia <o_melnikova@inbox.ru>
2019-11-29 09:56:53 +0400

    tests: part#2

--- __tests__/button-load.test.js
+++ __tests__/button-load.test.js
@@ -1,6 +1,4 @@
-const createButtonLoadTemplate = () => {
-  return `<button class="load-more" type="button">load more</button>`;
-};
+import {createButtonLoadTemplate} from '../src/components/button-load.js'
 
 test('create template of button load', () => {
   expect(createButtonLoadTemplate()).toBe(`<button class="load-more" type="button">load more</button>`);
--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,19 +1,4 @@
-import {createMenuTemplate} from '../src/components/menu.js'
-import {createFiltersTemplate} from '../src/components/filter.js'
-import {createTasksListTemplate} from '../src/components/tasks.js'
-import {createTaskItemTemplate} from '../src/components/task.js'
-import {createTaskItemEditTemplate} from '../src/components/task-edit.js'
-import {createButtonLoadTemplate} from '../src/components/button-load.js'
-
-
-const render = (container, template, place = `beforeEnd`) => {
-  container.insertAdjacentHTML(place, template);
-};
-
-const siteMainElement = document.querySelector(`.main`);
-const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
-
-render(siteHeaderElement, createMenuTemplate());
+import {main} from '../src/main.js'
 
 test('create menu', () => {
   expect(siteHeaderElement.querySelector(`.control__btn-wrap`).length).toBe(1);
