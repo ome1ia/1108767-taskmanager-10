ad3b6b945473d21448c2ca5515165ef1fb33839a
ome1ia <o_melnikova@inbox.ru>
2019-12-04 09:57:02 +0400

    mock & variables in task

--- src/components/task.js
+++ src/components/task.js
@@ -1,5 +1,91 @@
+import {getTask} from '../mock/task.js';
+
+let {description, dueDate, repeatingDays, tags, color} = getTask();
+
+const setDay = (date) => {
+  const months = [
+    `January`,
+    `February`,
+    `March`,
+    `April`,
+    `May`,
+    `June`,
+    `July`,
+    `August`,
+    `September`,
+    `October`,
+    `November`,
+    `December`];
+
+  let day = date.getDate();
+  let month = months[date.getMonth()];
+
+  return `${day} ${month}`;
+};
+
+const castTimeFormat = (value) => {
+  return value < 10 ? `0${value}` : String(value);
+};
+
+const setTime = (date) => {
+  const hours = castTimeFormat(date.getHours() % 12);
+  const minutes = castTimeFormat(date.getMinutes());
+
+  const interval = date.getHours() > 11 ? `PM` : `AM`;
+
+  return `${hours}:${minutes} ${interval}`;
+};
+
+const setTags = (hashtags) => {
+  let template = ``;
+  for (let hashtag of hashtags) {
+    template += `<span class="card__hashtag-inner">
+                          <span class="card__hashtag-name">
+                            #${hashtag}
+                          </span>
+                        </span>`;
+  }
+  return template;
+};
+
+const checkRepeating = (days) => {
+  let repeatingDay = ``;
+  for (let day in days) {
+    if (days[day]) {
+      repeatingDay += day;
+      break;
+    }
+  }
+  return repeatingDay;
+};
+
+const checkDeadline = (date) => {
+  let nowDate = new Date();
+  return (nowDate > date);
+};
+
+// я может что-то не понимаю: В ТЗ: "Дни недели повтора задачи: отображено,
+// если установлен флаг повторения. В противном случае скрыто."
+// в верстке /1108767-taskmanager-10/markup/index.html время выводится, добавлен класс
+
+let taskDate;
+let taskTime;
+
+if (checkRepeating(repeatingDays)) {
+  taskDate = checkRepeating(repeatingDays);
+  taskTime = ``;
+} else {
+  taskDate = dueDate ? setDay(dueDate) : ``;
+  taskTime = dueDate ? setTime(dueDate) : ``;
+}
+
+let taskTags = setTags(tags);
+let isDeadline = checkDeadline(dueDate);
+let isDeadlineClass = isDeadline ? `card--deadline` : ``;
+
+
 export const createTaskItemTemplate = () => {
-  return `<article class="card card--black">
+  return `<article class="card card--${color} ${isDeadlineClass}">
             <div class="card__form">
               <div class="card__inner">
                 <div class="card__control">
@@ -24,7 +110,7 @@ export const createTaskItemTemplate = () => {
                 </div>
 
                 <div class="card__textarea-wrap">
-                  <p class="card__text">Example default task with default color.</p>
+                  <p class="card__text">${description}</p>
                 </div>
 
                 <div class="card__settings">
@@ -32,31 +118,15 @@ export const createTaskItemTemplate = () => {
                     <div class="card__dates">
                       <div class="card__date-deadline">
                         <p class="card__input-deadline-wrap">
-                          <span class="card__date">23 September</span>
-                          <span class="card__time">11:15 PM</span>
+                          <span class="card__date">${taskDate}</span>
+                          <span class="card__time">${taskTime}</span>
                         </p>
                       </div>
                     </div>
 
                     <div class="card__hashtag">
                       <div class="card__hashtag-list">
-                        <span class="card__hashtag-inner">
-                          <span class="card__hashtag-name">
-                            #todo
-                          </span>
-                        </span>
-
-                        <span class="card__hashtag-inner">
-                          <span class="card__hashtag-name">
-                            #personal
-                          </span>
-                        </span>
-
-                        <span class="card__hashtag-inner">
-                          <span class="card__hashtag-name">
-                            #important
-                          </span>
-                        </span>
+                        ${taskTags}
                       </div>
                     </div>
                   </div>
---
+++ src/mock/task.js
@@ -0,0 +1,100 @@
+const getRandom = (max, min = 0) => {
+
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+
+};
+
+const getTaskDescription = () => {
+  const taskDescriptions = [`Изучить теорию`, `Сделать домашку`, `Пройти интенсив на соточку`];
+  return taskDescriptions[getRandom(taskDescriptions.length - 1)];
+};
+
+const getDate = () => {
+  let targetDate;
+  let dateDiff;
+
+  if (Math.random() > 0.7) {
+    targetDate = null;
+  } else {
+    targetDate = new Date();
+    dateDiff = getRandom(7, -7);
+    targetDate.setDate(targetDate.getDate() + dateDiff);
+  }
+
+  return targetDate;
+};
+
+const getDay = () => {
+  const days = {
+    'mo': false,
+    'tu': false,
+    'we': false,
+    'th': false,
+    'fr': false,
+    'sa': false,
+    'su': false
+  };
+
+  const daysList = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
+  let repeatingDayIndex = getRandom(8);
+  let repeatingDay = daysList[repeatingDayIndex];
+
+  if (repeatingDay) {
+    days[repeatingDay] = true;
+  }
+
+  // в задании написано объект, но я бы возвращала строку (пустую или с названием дня)
+
+  return days;
+};
+
+const getTags = () => {
+  const tagsList = [`homework`, `theory`, `practice`, `intensive`, `keks`];
+  let tagsSize = getRandom(3);
+  let tags = new Set();
+
+  for (let i = 0; i <= tagsSize; i++) {
+    let tagId = getRandom(tagsSize - 1);
+    tags.add(tagsList[tagId]);
+  }
+
+  return tags;
+};
+
+const getColor = () => {
+  const colors = [`black`, `yellow`, `blue`, `green`, `pink`];
+  return colors[getRandom(4)];
+};
+
+const getIsFavorite = () => {
+  return !!(getRandom(1));
+};
+
+const getIsArchive = () => {
+  return !!(getRandom(1));
+};
+
+const getTask = () => {
+  return {
+    description: getTaskDescription(),
+    dueDate: getDate(),
+    repeatingDays: getDay(),
+    tags: getTags(),
+    color: getColor(),
+    isFavorite: getIsFavorite(),
+    isArchive: getIsArchive()
+  };
+};
+
+const TASK_COUNT = 3;
+
+const getTasks = () => {
+  let tasks = [];
+  for (let i = 0; i < TASK_COUNT; i++) {
+    tasks.push(getTask());
+  }
+  return tasks;
+};
+
+export {getTask, getTasks};
